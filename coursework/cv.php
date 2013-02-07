65e07c9399b2c6d1de1c4e277dd1158991417f70
Zac Colley <hi@zaccolley.com>
2013-02-07 13:29:45 +0000

    Fleshed more out with more content

--- coursework/cv.php
+++ coursework/cv.php
@@ -4,8 +4,8 @@
 
 		<section class="details">
 			<h1>Zac Colley</h1>
-			<p>✉ <a href="emailto:z@colley.com">z@accolley.com</a></p>
-			<p>☎ 07534317671</p>
+			<p>&#009993; <a href="emailto:z@colley.com">z@accolley.com</a></p>
+			<p>&#009742; 07534317671</p>
 			<p><a href="http://www.zaccolley.com/">zaccolley.com</a></p>
 			<p class="address">70 Stansted Road,
 			   Portsmouth,
@@ -14,6 +14,89 @@
 			   PO5 1SA
 			</p>
 		</section>
+
+		<section class="content I">
+			<h2>I&hellip;</h2>
+			<ul>
+				<li>strive to make my work something I am proud of.</li>
+				<li>am flexible to change and learn quickly.</li>
+				<li>will use my intuitive, experience and resources to try and solve problem.</li>
+				<li>have a good sense of design and importance for detail.</li>
+				<li>work well with others and can find someone’s expertise and work with that.</li>
+			</ul>
+		</section>
+
+		<section class="content">
+			<h2>Education</h2>
+		</section>
+
+		<section class="content">
+			<h2>Qualifications</h2>
+			<h3>GSCE</h3>
+				<ul>
+					<li>9 including:</li>
+					<li>Maths (A)</li>
+					<li>English (A)</li>
+				</ul>
+			<h3>GCE Advanced Level</h3>
+				<ul>
+					<li>Mathematics (B)</li>
+					<li>Physics (C)</li>
+					<li>Computing (C)</li>
+				</ul>
+		</section>
+
+		<section class="content">
+			<h2>Skills</h2>
+			<h3>Competent with:</h3>
+
+			<!--
+			HTML (HTML5)
+			CSS (CSS3)
+			Experience with:
+
+			Python
+			Javascript (including jQuery)
+			PHP
+			SQL (MySQL usage)
+			Git
+			Java
+			-->
+		</section>
+
+		<section class="content">
+			<h3>Experience</h3>
+			<!--
+
+			What?	When?	Skills shown:
+			Web Section Head at UPRacing 	Present	
+			Leading projects
+			Organisation
+			An eye for design
+			Sales Assistant at Dunelm Mill	2011
+
+			Working in a team
+			Time management
+			Customer handling	
+			Work Experience at AT&T 	2010	
+			Flexibility
+			-->
+		</section>
+
+		<section class="content">
+			<h3>Interests</h3>
+			<!--
+			I play guitar:
+
+			I have been for six years.
+			Playing with others is a joy.
+			    I love music:
+
+			 Sharing music is great.
+			I have passion for the web.
+			-->
+		</section>
+
 	</article>
 
 </body>
