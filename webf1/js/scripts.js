08815a40b86d0d9f7692756870f2ef3a499ad31c
Zac Colley <hi@zaccolley.com>
2013-02-16 13:36:57 +0000

    Scrolling Javascript and referencing linking

--- webf1/js/scripts.js
+++ webf1/js/scripts.js
@@ -1,4 +1,4 @@
-$(function(){		
+$(function(){
 
 	// cv scripts
 	// ----------
@@ -23,22 +23,31 @@ $(function(){
 
 	}	
 
-	// worklog scripts
+	// scrolling scripts
 	// ---------------
 
-	var animationSpeed = 1000
+	// link that takes you to the top
+	$('.toTheTop').click(function(){ scrollPage(this); });
 
-	$('a.toTheTop').click(function(){
-		$('html, body').animate({scrollTop: 0}, animationSpeed); // Moves to the top of the window in 'animationSpeed'ms
-		event.preventDefault();  // Stops the link's normal behaviour
-	});
+	// worklog nav 
+	$('.worklog li.active a').click(function(){	scrollPage(this); });
 
-	$('li.active a').click(function(){				
-		var hrefValue = $(this).attr('href'); // Grabs the href attribute from the clicked navigation link
-		var postPosition = $(hrefValue).position().top;	// Finds the position from the top of the window for the heading with the ID 'hrefValue'
-		var scrollAmount = postPosition - 5;  // -5 to move to above the heading at the top of the post
-		$('html, body').animate({scrollTop: scrollAmount}, animationSpeed); // Moves to the top of the post in 'animationSpeed'ms
+ 	// inline referencing in the lit review
+	$('.litreview mark a').click(function(){ scrollPage(this); });
+
+	function scrollPage($clicked) {
+		$animationSpeed = 1000 // Speed of the animation in ms
+		$url = window.location.protocol + "//" + window.location.host + window.location.pathname; // Get current URL
+		$id = String($clicked).substr($url.length); // Take the URL and leave the # part
+		$headerHeight = 100; // Height of header
+		if($id == "#"){ // If the '.toTheTop' is clicked
+			$scrollAmount = 0; // The top of the page
+		}else{ // If the '.toTheTop' hasn't been clicked
+			$postPosition = $($id).position().top;	// Finds the position from the top of the window for the heading with the ID 'hrefValue'
+			$scrollAmount = $postPosition - $headerHeight;  // How far scrolled down minus the height of the header
+		}
+		$('html, body').animate({scrollTop: $scrollAmount}, $animationSpeed); // Moves to the top of the post in 'animationSpeed'ms
 		event.preventDefault(); // Stops the link's normal behaviour
-	});
+	}
 
 });
