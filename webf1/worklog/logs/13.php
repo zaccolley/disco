4ea8c1f97a08a51691046faa530357847c54ce1f
Zac Colley <hi@zaccolley.com>
2013-03-13 13:16:49 +0000

    Changed the image function and added more content

--- webf1/worklog/logs/13.php
+++ webf1/worklog/logs/13.php
@@ -2,10 +2,10 @@
 
 	$text = "<p>I have used the waterfall method before on projects at college and so was familiar with this.</p>"
 		  . "<p>I had used rapid prototyping as a method of coding before but didn't know it had a name.</p>"
-		  . "<p>I've decided to use git for my version control on the project and use GitHub to help me keep track of the code. GitHub has a system called 'issues' which is essentially a to-do list. I'll use this to track my tasks and work through them.</p>"
-		  . createImage('http://www.github.com/zaccolley', 'img/githubscreen.png', 'Github Issues screenshot', 'center')
+		  . "<p>I've decided to use git for my version control on the project and use " . createLink('http://www.github.com/zaccolley', 'GitHub', 'My GitHub profile') . " to help me keep track of the code. GitHub has a system called 'issues' which is essentially a to-do list. I'll use this to track my tasks and work through them.</p>"
+		  . createImage('img/githubscreen.png', 'Github Issues screenshot', 'center')
 		  . "<p>Here are some of my commits from the repo:</p>"
-		  . createImage('http://www.github.com/zaccolley', 'img/githubcommitsscreen.png', 'Github Issues screenshot', 'center');
+		  . createImage('img/githubcommitsscreen.png', 'Github Issues screenshot', 'center');
 
 	echo createPost("Project Management Issues", "13", strtotime("January 08 2013 15:00:00 GMT"), $text);
 
--- webf1/worklog/logs/16.php
+++ webf1/worklog/logs/16.php
@@ -1,7 +1,8 @@
 <?php
 
-	$text = "<p></p>";
+	$text = "<p><em>Note: I missed this lecture. :¬(</em></p>"
+		  . "<p>I do love me some mobile devices though.</p>";
 
-	echo createPost("Usability 1", "16", strtotime("January 29 2013 15:00:00 GMT"), $text);
+	echo createPost("Mobile Devices, Mobility & the Mobile Web", "16", strtotime("January 29 2013 15:00:00 GMT"), $text);
 
 ?>
