4f8825b27ce7f10909737c65fbc0c07652b51b34
Zac Colley <hi@zaccolley.com>
2013-03-14 15:42:57 +0000

    Spelling corrections #8

--- webf1/worklog/logs/1.php
+++ webf1/worklog/logs/1.php
@@ -3,7 +3,7 @@
 	$text = "<p>Not a lot happened, prepping my USB stick with "
 			  . createAbbr('XAMPP', 'X (cross platform), Apache HTTP Server, MySQL, PHP, Perl')
 			  . " and the " . createLink('http://portableapps.com/', 'Portable Apps', 'Portable Apps') . " suite.</p>"
-			  . "<p>Lecture was fairly interesting, I didn't know a lot about internet browser history. The <q>Browser Wars</q>"
+			  . "<p>Lecture was fairly interesting, I didn't know a lot about Internet browser history. The <q>Browser Wars</q>"
 			  . "have gone on for longer than I though too. "
 			  . "I did know a fair bit about browsers prior to the lecture from doing some web development in my spare time.</p>";
 
