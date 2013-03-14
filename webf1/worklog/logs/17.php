c46c4b47d4b9d27af6fe2f9edf384dd7f8eb23ce
Zac Colley <hi@zaccolley.com>
2013-03-14 13:51:21 +0000

    Added worklog 17 & 18. Closes #16

--- webf1/worklog/logs/17.php
+++ webf1/worklog/logs/17.php
@@ -1,6 +1,6 @@
 <?php
 
-	$text = "<p></p>";
+	$text = "<p>My navigation is at the top of the screen in the header as I felt this would be this best place to put it. If I were to improve the nav I would have mad it so it could link to any site easily from any other page. I'm not sure I'd achieve this but I feel it'd be a lot better for navigation.</p>";
 
 	echo createPost("Usability 1", "17", strtotime("February 05 2013 15:00:00 GMT"), $text);
 
