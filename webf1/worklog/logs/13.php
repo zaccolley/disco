d3a858786fa4e8f044b53f5485397f4a57f04ffc
Zac Colley <hi@zaccolley.com>
2013-03-14 15:04:08 +0000

    Slight change to worklog and adding references to log

--- webf1/worklog/logs/13.php
+++ webf1/worklog/logs/13.php
@@ -5,7 +5,8 @@
 		  . "<p>I've decided to use git for my version control on the project and use " . createLink('http://www.github.com/zaccolley', 'GitHub', 'My GitHub profile') . " to help me keep track of the code. GitHub has a system called 'issues' which is essentially a to-do list. I'll use this to track my tasks and work through them.</p>"
 		  . createImage('img/githubscreen.png', 'Github Issues screenshot', '')
 		  . "<p>Here are some of my commits from the repo:</p>"
-		  . createImage('img/githubcommitsscreen.png', 'Github Issues screenshot', '');
+		  . createImage('img/githubcommitsscreen.png', 'Github Issues screenshot', '')
+		  . "<p><em>I also used some other things in this project which are " . createLink("references.txt", "found in my references", "References") . ".</em></p>";
 
 	echo createPost("Project Management Issues", "13", strtotime("January 08 2013 15:00:00 GMT"), $text);
 
