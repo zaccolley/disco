baac4559169423a8bca2cba8f8b9bc51c54f3cbf
Zac Colley <hi@zaccolley.com>
2013-04-22 17:22:07 +0100

    Added players son

---
+++ year1/indadd/players.sql
@@ -0,0 +1,8 @@
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('1', 'Samir', 'Ujkani', 'GK', '0');
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('2', 'Andi', 'Lila', 'DF', '0');
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('3', 'Lorik', 'Cana', 'MF', '0');
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('4', 'Edgar', 'Cani', 'FW', '0');
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('5', 'Josep', 'Gomes', 'GK', '1');
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('6', 'Ildefons', 'Lima', 'DF', '1');
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('7', 'Oscar', 'Sonejee', 'MF', '1');
+INSERT INTO `WorldCupSystem`.`Player` (`Play_ID`, `Play_Forename`, `Play_Surname`, `Play_Position`, `Team_ID`) VALUES ('8', 'Fernando', 'Silva', 'FW', '1');
