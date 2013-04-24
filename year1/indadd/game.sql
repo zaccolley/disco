4a98cd24b08a6e20e6a12af5a70e3acedacc1fe9
Zac Colley <hi@zaccolley.com>
2013-04-24 13:34:22 +0100

    Add goals yo

--- year1/indadd/game.sql
+++ year1/indadd/game.sql
@@ -4,9 +4,9 @@ INSERT INTO WorldCupSystem.Game VALUES ('2', '2014-06-14 13:30:00', '05:01', '00
 INSERT INTO WorldCupSystem.Game VALUES ('3', '2014-06-15 14:30:00', '03:00', '0000-00-00 00:00:01', '1', '3');
 INSERT INTO WorldCupSystem.Game VALUES ('4', '2014-06-16 18:30:00', '00:00', '0000-00-00 00:00:00', '1', '4');
 INSERT INTO WorldCupSystem.Game VALUES ('5', '2014-06-17 17:30:00', '03:02', '0000-00-00 00:00:02', '1', '5');
-INSERT INTO WorldCupSystem.Game VALUES ('6', '2014-06-18 15:30:00', '04:04', '0000-00-00 00:00:00', '1', '6');
+INSERT INTO WorldCupSystem.Game VALUES ('6', '2014-06-18 15:30:00', '01:01', '0000-00-00 00:00:00', '1', '6');
 INSERT INTO WorldCupSystem.Game VALUES ('7', '2014-06-19 12:30:00', '02:01', '0000-00-00 00:00:05', '1', '7');
-INSERT INTO WorldCupSystem.Game VALUES ('8', '2014-06-20 13:30:00', '05:02', '0000-00-00 00:00:04', '1', '8');
+INSERT INTO WorldCupSystem.Game VALUES ('8', '2014-06-20 13:30:00', '00:00', '0000-00-00 00:00:04', '1', '8');
 INSERT INTO WorldCupSystem.Game VALUES ('9', '2014-06-21 14:30:00', '01:01', '0000-00-00 00:00:00', '1', '9');
-INSERT INTO WorldCupSystem.Game VALUES ('10', '2014-06-22 15:30:00', '04:03', '0000-00-00 00:00:03', '1', '10');
-INSERT INTO WorldCupSystem.Game VALUES ('11', '2014-06-23 15:30:00', '05:01', '0000-00-00 00:00:00', '1', '11');
+INSERT INTO WorldCupSystem.Game VALUES ('10', '2014-06-22 15:30:00', '03:01', '0000-00-00 00:00:03', '1', '10');
+INSERT INTO WorldCupSystem.Game VALUES ('11', '2014-06-23 15:30:00', '00:00', '0000-00-00 00:00:00', '1', '11');
