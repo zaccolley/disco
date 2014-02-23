c5ec6e9f296cccc13774b1a19de3b1a71804a099
Zac Colley <hi@zaccolley.com>
2014-02-23 19:59:48 +0000

    Added hospital insert data

---
+++ year2/dbprin/sub2/insertdata.sql
@@ -0,0 +1,26 @@
+-- Insert data for the Happy Hospital system
+
+-- Hospitals
+
+INSERT INTO Hospital VALUES
+(0, "Amersham Hospital", "Amersham", "Buckinghamshire"),
+(1, "Coldeast Hospital", "Sarisbury Green", "Hampshire"),
+(2, "Fordingbridge Hospital", "Fordingbridge", "Hampshire"),
+(3, "Gosport War Memorial Hospital ", "Gosport", "Hampshire"),
+(4, "Havant War Memorial Hospital ", "Havant", "Hampshire"),
+(5, "Heatherwood Hospital", "Ascot"," Berkshire"),
+(6, "Horton General Hospital", "Banbury", "Oxfordshire"),
+(7, "Moorgreen Hospital", "West End", "Hampshire"),
+(8, "Netley Hospital", "Netley", "Hampshire"),
+(9, "Petersfield Hospital", "Petersfield", "Hampshire"),
+(10, "Queen Alexandra Hospital", "Portsmouth", "Hampshire"),
+(11 , "Royal Berkshire Hospital", "Reading", "Berkshire"),
+(12 , "Royal Buckinghamshire Hospital", "Aylesbury", "Buckinghamshire"),
+(13 , "Royal Hospital Haslar", "Gosport", "Hampshire"),
+(14 , "Roundway Hospital", "Devizes", "Wiltshire"),
+(15 , "Stoke Mandeville Hospital", "Aylesbury", "Buckinghamshire"),
+(16 , "St Christopher's Hospital", "Fareham", "Hampshire"),
+(17 , "St James' Hospital", "Portsmouth", "Hampshire"),
+(18 , "St Mary's Hospital", "Portsmouth", "Hampshire"),
+(19 , "Wexham Park Hospital", "Slough", "Berkshire"),
+(20 , "Wycombe General Hospital", "High Wycombe", "Buckinghamshire");
