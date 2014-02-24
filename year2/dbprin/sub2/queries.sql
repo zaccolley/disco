fc38a28ccbe5f711d2375d2257390d09d09cf7c0
Zac Colley <hi@zaccolley.com>
2014-02-24 13:29:58 +0000

    Updated queries

--- year2/dbprin/sub2/queries.sql
+++ year2/dbprin/sub2/queries.sql
@@ -17,20 +17,19 @@ Query 1
 CREATE VIEW Nurses AS
 
 SELECT
-CONCAT_WS('. ',PERSON_TITLE,SUBSTR(PERSON_FNAME, 1, 1),PERSON_SNAME) AS 'Name',
-PERSON_TELEPHONE AS 'Telephone No.',
-CONCAT(WARD_NAME,' (',DEPARTMENT_NAME,')') AS 'Ward (Department)'
+   CONCAT_WS('. ',PERSON_TITLE,SUBSTR(PERSON_FNAME, 1, 1),PERSON_SNAME) AS 'Name',
+   PERSON_TELEPHONE AS 'Telephone No.',
+   CONCAT(WARD_NAME,' (',DEPARTMENT_NAME,')') AS 'Ward (Department)'
 
 FROM
-Nurse, Person, Staff, Ward, Department
+   Nurse, Person, Staff, Ward
 
 WHERE
-Nurse.STAFF_ID = Staff.STAFF_ID AND 
-Staff.PERSON_ID = Person.PERSON_ID AND
-Nurse.WARD_ID = Ward.WARD_ID AND
-Ward.DEPARTMENT_ID = Department.DEPARTMENT_ID AND
-Department.DEPARTMENT_ID = 6
+   Nurse.PERSON_ID = Person.PERSON_ID AND
+   Nurse.WARD_ID = Ward.WARD_ID AND
+   Ward.WARD_ID = 6
 
+GROUP BY PERSON_SNAME
 ORDER BY PERSON_SNAME DESC;
 
 /*
@@ -48,20 +47,18 @@ Query 2
 */
 
 SELECT
-CONCAT_WS('. ',SUBSTR(PERSON_FNAME, 1, 1),PERSON_SNAME) AS 'Name',
--- CONCAT_WS(', ',PERSON_HOUSENONAME, PERSON_STREET, PERSON_TOWNCITY, PERSON_COUNTY) AS Address,
--- DATE_FORMAT(PERSON_DOB, '%D %M %Y (%d/%c/%Y)') AS 'Date of Birth',
-UPPER(PERSON_EMAIL) AS 'Email',
-HOSPITAL_NAME AS 'Hospital'
+   CONCAT_WS('. ',SUBSTR(PERSON_FNAME, 1, 1),PERSON_SNAME) AS 'Name',
+   UPPER(PERSON_EMAIL) AS 'Email Address',
+   HOSPITAL_NAME AS 'Hospital Name'
 FROM
-Person, Patient, Ward, Hospital
+   Person, Patient, Ward, Hospital
 
 WHERE
-Patient.PERSON_ID = Person.PERSON_ID AND
-Patient.WARD_ID = Ward.WARD_ID AND
-Ward.HOSPITAL_ID = Hospital.HOSPITAL_ID AND 
-Hospital.HOSPITAL_ID = 10 AND
-PERSON_EMAIL NOT LIKE '%@%.%'
+   Patient.PERSON_ID = Person.PERSON_ID AND
+   Patient.WARD_ID = Ward.WARD_ID AND
+   Ward.HOSPITAL_ID = Hospital.HOSPITAL_ID AND 
+   Hospital.HOSPITAL_ID = 10 AND
+   PERSON_EMAIL NOT LIKE '%@%.%'
 
 ORDER BY PERSON_EMAIL ASC;
 
@@ -69,12 +66,42 @@ ORDER BY PERSON_EMAIL ASC;
 
 SELECT COUNT(P.PERSON_EMAIL) AS 'Amount of Invalid Emails' FROM
 (
-SELECT PERSON_EMAIL FROM Person, Patient, Ward, Hospital
-WHERE
-Patient.PERSON_ID = Person.PERSON_ID AND
-Patient.WARD_ID = Ward.WARD_ID AND
-Ward.HOSPITAL_ID = Hospital.HOSPITAL_ID AND 
-Hospital.HOSPITAL_ID = 10 AND
-PERSON_EMAIL NOT LIKE '%@%.%'
+   SELECT PERSON_EMAIL FROM Person, Patient, Ward, Hospital
+   WHERE
+      Patient.PERSON_ID = Person.PERSON_ID AND
+      Patient.WARD_ID = Ward.WARD_ID AND
+      Ward.HOSPITAL_ID = Hospital.HOSPITAL_ID AND 
+      Hospital.HOSPITAL_ID = 10 AND
+      PERSON_EMAIL NOT LIKE '%@%.%'
 ) P;
 
+/*
+
+Query 3
+--------
+
++ Find a list of all patients and their data
+
+- Set initial as forename
+- Set email to uppercase
+- Nice titled columns
+
+*/
+
+SELECT
+   CONCAT_WS('. ',SUBSTR(PERSON_FNAME, 1, 1),PERSON_SNAME) AS 'Name',
+   PERSON_GENDER AS 'Gender',
+   CONCAT_WS(', ',PERSON_HOUSENONAME, PERSON_STREET, PERSON_TOWNCITY, PERSON_COUNTY) AS Address,
+   DATE_FORMAT(PERSON_DOB, '%D %M %Y (%d/%c/%Y)') AS 'Date of Birth',
+   PERSON_TELEPHONE AS 'Telephone Number',
+   UPPER(PERSON_EMAIL) AS 'Email Address',
+   HOSPITAL_NAME AS 'Hospital Name'
+FROM
+   Person, Patient, Ward, Hospital
+
+WHERE
+   Patient.PERSON_ID = Person.PERSON_ID AND
+   Patient.WARD_ID = Ward.WARD_ID AND
+   Ward.HOSPITAL_ID = Hospital.HOSPITAL_ID 
+
+ORDER BY PERSON_FNAME ASC;
