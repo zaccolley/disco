0fe387edd6aff1f50adca0627396a780ab33278a
Zac Colley <hi@zaccolley.com>
2014-03-10 17:10:16 +0000

    Added timer

--- second/gude/webapp/js/main.js
+++ second/gude/webapp/js/main.js
@@ -1,15 +1,71 @@
 $('document').ready(function(){
 
-	$('.timer-button').click(function(){
-		console.log('Clicked timer')
-		if($(this).hasClass('btn-negative')){
-			$(this).text('Start timer');
+	timerInit();
+
+});
+
+function timerInit(){
+
+	console.log("Start timer");
+
+	$('.startstop-timer').click(function(){
+		console.log('Clicked timer', $(this).attr('data-state'));
+
+		if($(this).attr('data-state') == "stopped"){
+			timerStart();
 		}else{
-			$(this).text('Stop timer');
+			timerStop();
 		}
-		$(this).toggleClass('btn-positive');
-		$(this).toggleClass('btn-negative');
 
 	});
 
-});
+	$('.clear-timer').click(timerClear);
+}
+
+function timerStop(){
+
+	console.log("Stop timer");
+	clearInterval(timer);
+
+	$('.startstop-timer').text('Start timer');
+	$('.startstop-timer').attr('data-state', "stopped");
+
+	$('.startstop-timer').removeClass('btn-primary');
+	$('.startstop-timer').addClass('btn-positive');
+
+}
+
+function timerStart(){
+
+	$('.startstop-timer').text('Pause timer');
+	$('.startstop-timer').attr('data-state', "running");
+
+	$('.startstop-timer').addClass('btn-primary');
+	$('.startstop-timer').removeClass('btn-positive');
+
+	// initial press
+	timerInc();
+	timer = window.setInterval(timerInc, 100);
+
+}
+
+function timerInc(){
+
+	console.log("Increment timer", $('.timer-display').text());
+
+	var currentTime = +$('.timer-display').text();
+	$('.timer-display').text(currentTime + 1);
+
+}
+
+function timerClear(){
+
+	console.log("Clear timer");
+
+	if(timer){
+		timerStop();
+
+		$('.timer-display').text('0');
+	}
+
+}
