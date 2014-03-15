24584319ffb3519a7a0fc6b0712985115b895cfa
Zac Colley <hi@zaccolley.com>
2014-03-15 15:02:53 +0000

    Updated timer

--- second/gude/webapp/js/timer.js
+++ second/gude/webapp/js/timer.js
@@ -1,68 +1,38 @@
+timerStates = ["In the shop?",
+			   "Received your order?",
+			   "Received a receipt?",
+			   "Out of the shop?"];
+
+timerStateCount = 1;
+
 function timerInit(){
 
 	console.log("Timer initialised");
 
-	$('.startstop-timer').on('touchend', function(){
-		console.log('Clicked timer', $(this).attr('data-state'));
-
-		if($(this).attr('data-state') == "stopped"){
-			timerStart();
-		}else{
-			timerStop();
+	$('.startstop-timer').on('click', function(){
+		
+		if($('.startstop-timer').hasClass('btn-positive')){
+			$('.timer').find('#sw_reset').trigger('click');
+			$('.timer').find('#sw_start').trigger('click');
+		}else if($('.startstop-timer').hasClass('btn-negative')){
+			timerStateCount = 0;
+			$('.timer').find('#sw_stop').trigger('click');
 		}
 
+		$('.startstop-timer').removeClass('btn-negative');
+		$('.startstop-timer').removeClass('btn-positive');
+		$('.startstop-timer').removeClass('btn-primary');
+
+		if(timerStateCount == 0){
+			$('.startstop-timer').addClass('btn-positive');
+		}else if(timerStateCount == timerStates.length-1){
+			$('.startstop-timer').addClass('btn-negative');
+		}else{
+			$('.startstop-timer').addClass('btn-primary');
+		}
+
+		$('.startstop-timer').text(timerStates[timerStateCount]);
+		timerStateCount++;
+
 	});
-
-	$('.clear-timer').on('touchend', timerClear);
-}
-
-function timerStop(){
-
-	console.log("Timer stopped");
-	
-	clearInterval(timer);
-
-	$('.startstop-timer').text('Start timer');
-	$('.startstop-timer').attr('data-state', "stopped");
-
-	$('.startstop-timer').removeClass('btn-primary');
-	$('.startstop-timer').addClass('btn-positive');
-	
-}
-
-function timerStart(){
-
-	console.log('Timer started');
-
-	$('.startstop-timer').text('Pause timer');
-	$('.startstop-timer').attr('data-state', "running");
-
-	$('.startstop-timer').addClass('btn-primary');
-	$('.startstop-timer').removeClass('btn-positive');
-
-	// initial press
-	timerInc();
-	timer = window.setInterval(timerInc, 100);
-
-}
-
-function timerInc(){
-
-	console.log("Timer incremented", $('.timer-display').text());
-
-	var currentTime = +$('.timer-display').text();
-	$('.timer-display').text(currentTime + 1);
-
-}
-
-function timerClear(){
-
-	console.log("Timer cleared");
-
-	if(timer){
-		timerStop();
-
-		$('.timer-display').text('0');
-	}
-
 }
