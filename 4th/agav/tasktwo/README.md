2553e72c33c4c21084637d68ea3411616a4a08ec
Zac Colley <hi@zaccolley.com>
2015-10-29 14:59:05 +0000

    Some more tests trying to get stable lines

--- 4th/agav/tasktwo/README.md
+++ 4th/agav/tasktwo/README.md
@@ -6,6 +6,7 @@ requires opencv version 2.4.x, 3.0 doesn't work with node-opencv atm
 
 1. install opencv `sudo apt-get install libopencv-dev`
 2. install node dependencies `npm install`
+3. create `resources_converted` directory
 
 # run
 
--- 4th/agav/tasktwo/app.js
+++ 4th/agav/tasktwo/app.js
@@ -1,21 +1,30 @@
 var cv = require('opencv');
 
 var inputPath = './resources/',
-    outputPath = './converted_resources/';
+    outputPath = './resources_converted/';
 
 var thresholds = {
   edges: {
-    low: 10,
-    high: 100
+    low: 50,
+    high: 200
   }
-}
+};
+
+var debug = true;
+
+var nIters = 2;
+var maxArea = 2500;
+
+var GREEN = [0, 255, 0]; // B, G, R
+var WHITE = [255, 255, 255]; // B, G, R
+var RED   = [0, 0, 255]; // B, G, R
 
 var fileNames = [
   '001.jpg', '002.jpg', '003.jpg', '004.jpg', '005.jpg', '006.jpg', '007.jpg',
   '008.jpg', '009.jpg', '010.jpg', '011.jpg'
 ];
 
-for(var f = 0; f < fileNames.length; f++){
+for(var f = 0; f < 1; f++){
   var fileName = fileNames[f];
 
   processImage(fileName);
@@ -27,38 +36,56 @@ function processImage(filename){
     if (err){
       throw err;
     }
-    if (image.width() < 1 || image.height() < 1){
+
+    var width = image.width();
+    var height = image.height();
+    if (width < 1 || height < 1){
       throw new Error('Image has no size');
     }
 
-    image.save(outputPath + fileName);
+    var big = new cv.Matrix(height, width);
+    var all = new cv.Matrix(height, width);
+
+    var originalImage = image.copy();
 
     // convert image to grayscale
     image.convertGrayscale();
-    image.save(outputPath + 'gray_' + fileName);
+    image.brightness(100);
+    debug && image.save(outputPath + 'gray_' + fileName);
 
     // blur it
-    for(var i = 0; i < 3; i++){
-      image.gaussianBlur([5, 5]);
-    }
-    image.save(outputPath + 'blurred_' + fileName);
+    image.medianBlur(5);
+    image.gaussianBlur([5, 5]);
+    debug && image.save(outputPath + 'blurred_' + fileName);
 
     // edge detection using the canny algorithm
     image.canny(thresholds.edges.low, thresholds.edges.high);
-    image.save(outputPath + 'edges_' + fileName);
+    debug && image.save(outputPath + 'edges_' + fileName);
 
-    // erode lines
-    image.erode(.8);
-    image.save(outputPath + 'eroded_' + fileName);
-
-    // dilate lines
-    image.dilate(.8);
-    image.save(outputPath + 'dilated_' + fileName);
+    // remove noise lines
+    image.dilate(1, 2);
+    image.erode(1, 2);
+    image.dilate(1, 1);
+    image.erode(1, 1);
+    debug && image.save(outputPath + 'denoised_' + fileName);
 
     // fix the lines using hough
     // -- (gray, lines, rho, theta, threshold, minLineLength, maxLineGap)
-    image.houghLinesP()
-    image.save(outputPath + 'lines_' + fileName);
+    var houghLines = image.houghLinesP()
+
+    for(var h = 0; h < houghLines.length; h++){
+      var houghLine = houghLines[h];
+
+      var x1 = houghLine[0];
+      var y1 = houghLine[1];
+      var x2 = houghLine[2];
+      var y2 = houghLine[3];
+
+      originalImage.line([x1,y1], [x2, y2]);
+
+    }
+
+    originalImage.save(outputPath + 'lines_' + fileName);
 
     console.log('Images saved to: ' + outputPath);
 
