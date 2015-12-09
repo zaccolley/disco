61f92ea045867bd5b48e8d84ffe583b80f77783a
Zac Colley <hi@zaccolley.com>
2015-12-09 18:07:57 +0000

    Added vehicle seeding

--- 4th/dwm/1/dbseed.js
+++ 4th/dwm/1/dbseed.js
@@ -1,47 +1,125 @@
 var faker = require('faker'),
     mysql = require('mysql'),
-    vehicles = require('../vehicles');
+    vehicles = require('./vehicles');
 
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database : 'dwm'
+  database : 'dwm1'
 });
 
-function getRandomVehicle(){
+console.log('Seeding database...');
+
+function randomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min) + +min);
+}
+
+// -- Vehicle seeding
+
+function genRandomEngineSize() {
+  var minEngineSize = 1000;
+  var maxEngineSize = 5000;
+  return randomBetween(minEngineSize, maxEngineSize) + 'cc';
+}
+
+function genRandomColour() {
+
+  var colours = vehicles.colours;
+
+  var randomPercentage = randomBetween(colours[0].percentage, colours[colours.length - 1].percentage);
+
+  for (var i = 0; i < colours.length; i++) {
+    var colour = colours[i];
+    if (randomPercentage < colour.percentage) {
+      return colour.name;
+    }
+  }
+
+  return colours[colours.length - 1].name;
+}
+
+function getNumberPlate(year) {
+  // based on: https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_the_United_Kingdom,_Crown_dependencies_and_overseas_territories#Local_memory_tags
+
+  var numberPlate = '';
+
+  // first char is for local area
+  var firstChar = 'ABCDEFGHKLMNOPRSVWXY';
+  numberPlate += firstChar.charAt(Math.floor(Math.random() * firstChar.length));
+
+  // second char is from the dvla
+  var secondChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  numberPlate += secondChar.charAt(Math.floor(Math.random() * secondChar.length));
+
+  // get the last two chars of the manufactured year
+  numberPlate += String(year).slice(-2);
+
+  // add a space
+  numberPlate += ' ';
+
+  // three random chars to top it off. I and Q are removed as they can create offensive words
+  var randomChars = 'ABCDEFGHJKLMNOPRSTUVWXYZ';
+  for (var i = 0; i < 3; i++){
+    numberPlate += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+
+  return numberPlate;
+}
+
+function getRandomVehicle() {
+
+  // generate some dates to use
+  var manufacturedDate = new Date(randomBetween(new Date('1980-01-01'), new Date('2003-01-01')));
+  var taxDate = new Date(randomBetween(new Date('2016-01-01'), new Date('2016-12-31')));
+  var motDate = new Date(randomBetween(new Date('2016-01-01'), new Date('2016-12-31')));
+
   return {
-    colour: vehicles.colours[Math.floor(Math.random() * vehicles.colours.length)].name,
-    make:   vehicles.makes[Math.floor(Math.random() * vehicles.makes.length)]
+    numberPlate: getNumberPlate(manufacturedDate.getFullYear()),
+    make: vehicles.makes[randomBetween(0, vehicles.makes.length - 1)],
+    colour: genRandomColour(),
+    engineSize: genRandomEngineSize(),
+    fuelType: vehicles.fuelTypes[randomBetween(0, vehicles.fuelTypes.length - 1)],
+    typeApproval: vehicles.typeApprovals[randomBetween(0, vehicles.typeApprovals.length -1)],
+    yearOfManufacture: manufacturedDate.getFullYear(),
+    dateRegistered: manufacturedDate.toISOString(),
+    taxExpires: taxDate.toISOString(),
+    motExpires: motDate.toISOString()
   }
 }
 
-var vehicle = getRandomVehicle();
-var name = faker.name.findName();
-
-console.log(vehicle);
-console.log(name);
-
-// VehicleID 'CP03 OEF'
-// VehicleMake 'RENAULT'
-// VehicleColour 'RED'
-// VehicleEngineSize '1390cc'
-// VehicleFuelType 'PETROL'
-// VehicleStatus 'Tax not due'
-// VehicleTypeApproval 'M1'
-// VehicleYearOfManufacture 2003
-// VehicleDateRegistered 04 July 2003
-// VehicleTaxExpires 01 December 2016
-// VehicleMoTExpires 07 July 2016
-
 connection.connect();
 
-connection.query('SELECT * from DVLA', function(err, rows, fields) {
-  if (err) {
-    throw err;
-  }
+// var name = faker.name.findName();
 
-  console.log(rows);
-});
+console.log('Adding vehicles...');
 
-connection.end();
+var vehicleAmountToGenerate = 100;
+
+for (var i = 0; i < vehicleAmountToGenerate; i++) {
+  var vehicle = getRandomVehicle();
+
+  var sql = `
+    INSERT INTO Vehicle (
+      VehicleID, VehicleMake, VehicleColour, VehicleEngineSize,
+      VehicleFuelType, VehicleTypeApproval, VehicleYearOfManufacture,
+      VehicleDateRegistered, VehicleTaxExpires, VehicleMoTExpires
+    ) VALUES (
+      '${vehicle.numberPlate}', '${vehicle.make}', '${vehicle.colour}', '${vehicle.engineSize}',
+      '${vehicle.fuelType}', '${vehicle.typeApproval}', '${vehicle.yearOfManufacture}',
+      '${vehicle.dateRegistered}', '${vehicle.taxExpires}', '${vehicle.motExpires}'
+    )
+  `;
+
+  connection.query(sql, function(err, rows, fields) {
+    if (err) {
+      throw err;
+    }
+  });
+
+}
+
+console.log('Vehicles added!');
+
+console.log('Database seeded!');
+process.exit();
